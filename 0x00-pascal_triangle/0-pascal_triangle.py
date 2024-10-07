b2f83c493f975f3aa3fc73157f54bb39fb2754fd
Abdulbasit Abubakar <abdulbasit.oa7@gmail.com>
2024-10-07 15:39:00 +0100

    Flirting with the Pascal triangle
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   0-main.py
    	new file:   0-pascal_triangle.py
    	new file:   __pycache__/0-pascal_triangle.cpython-312.pyc
    
     Changes not staged for commit:
    	deleted:    ../0-main.py
    	deleted:    ../0-pascal_triangle.py
    	deleted:    ../__pycache__/0-pascal_triangle.cpython-312.pyc

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+def pascal_triangle(n):
+	if n <= 0:
+		return [] # To return an empty list is n<=0
+	pascal = [[1]] # For the first row
+	
+	for  a in range(1, n):
+		prev_row = pascal[-1] # To get the prev. row
+		curr_row = [1] # Begins the current row with 1
+		
+		# For the middle values of the row
+		for b in range(1, len(prev_row)):
+			curr_row.append(prev_row[b-1] + prev_row[b])
+
+		curr_row.append(1) # To end the row with 1
+		pascal.append(curr_row) # Adds the current row to P's triangle
+
+	return pascal
+
