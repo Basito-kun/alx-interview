b0787ab7e1c416c28dde6013d1621ded264be1ce
Abdulbasit Abubakar <abdulbasit.oa7@gmail.com>
2024-12-23 19:37:16 +0100

    Lockboxes: a method that determines if all boxes can be opened

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""
+A module to determine whether all boxes in a set of locked boxes can be opened.
+"""
+
 
 def canUnlockAll(boxes):
     """
     Determines whether all the boxes can be opened.
-    
+
     Args:
         boxes (list of lists): Each box is repr as a list of keys it contains.
-    
+
     Returns:
         bool: True if all boxes can be opened, False if vice versa.
     """
@@ -17,9 +21,8 @@ def canUnlockAll(boxes):
 
     while keys:
         key = keys.pop()  # Selects a key
-        if key < numb and not unlocked[key]:  # If it unlocks a valid and locked box
+        if key < numb and not unlocked[key]:  # If it unlocks a,locked box
             unlocked[key] = True  # Unlocks the box
             keys.update(boxes[key])  # Add new keys from this box
 
     return all(unlocked)  # Check whether all boxes are unlocked
-
